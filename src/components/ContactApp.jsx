926d577d4e3a4223f308d4dde4a110f304a1b52b
ryfazrin <riansera515@gmail.com>
2023-01-05 17:16:28 +0700

    Latihan Studi Kasus: Memproteksi Fitur kontak

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -5,7 +5,31 @@ import HomePage from '../pages/HomePage';
 import AddPage from '../pages/AddPage';
 
 class ContactApp extends React.Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      authedUser: null,
+    };
+  }
+
   render() {
+    if (this.state.authedUser === null) {
+      return (
+        <div className='contact-app'>
+          <header className='contact-app__header'>
+            <h1>Aplikasi Kontak</h1>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/*" element={<p>Halaman Login</p>} />
+              <Route path="/register" element={<p>Halaman Register</p>} />
+            </Routes>
+          </main>
+        </div>
+      )
+    }
+ 
     return (
       <div className="contact-app">
         <header className='contact-app__header'>
